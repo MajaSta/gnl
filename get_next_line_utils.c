cd91fae1edbb666ca40d2a0421fa10b74e629165
MajaSta <142170733+MajaSta@users.noreply.github.com>
2024-09-22 11:51:53 +0200

    Update get_next_line_utils.c

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -13,9 +13,6 @@
 #include "get_next_line.h"
 #include <stdlib.h>
 
-/*
- * Check if a newline exists in the linked list.
- */
 int found_newline(t_list *list)
 {
     int i;
@@ -33,11 +30,6 @@ int found_newline(t_list *list)
     return 0;
 }
 
-
-/*
- * Find and return the last node in the linked list.
- */
-
 t_list *find_last_node(t_list *list)
 {
     if (!list)
@@ -49,12 +41,6 @@ t_list *find_last_node(t_list *list)
     return list;
 }
 
-/*
- * Copy (string\n]
-*/
-/*
- * Copy the string until the newline into the provided buffer.
- */
 void copy_str(t_list *list, char *str)
 {
     int i, k = 0;
@@ -76,14 +62,6 @@ void copy_str(t_list *list, char *str)
     str[k] = '\0';
 }
 
-
-/*
- * find the len to new line in
- * my linked list
-*/
-/*
- * Find the length to newline in the linked list.
- */
 int len_to_newline(t_list *list)
 {
     int i, len = 0;
@@ -103,15 +81,6 @@ int len_to_newline(t_list *list)
     return len;
 }
 
-
-/*
- * dealloc all from head
- * set heat->NULL
-*/
-/*
- * Dealloc all from head and set head to NULL.
- * Optionally keep clean_node if it has data.
- */
 void dealloc(t_list **list, t_list *clean_node)
 {
     t_list *tmp;
@@ -127,7 +96,7 @@ void dealloc(t_list **list, t_list *clean_node)
         *list = tmp;
     }
     
-    *list = NULL;  // Clear the list pointer
+    *list = NULL;
 
     // If clean_node contains data, set it as the new head
     if (clean_node && clean_node->str_buf[0])
