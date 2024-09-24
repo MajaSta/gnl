2ab5765b1e341669a334941323645db4705663ea
MajaSta <142170733+MajaSta@users.noreply.github.com>
2024-09-24 13:41:35 +0200

    Create get_next_line_new.c

---
+++ get_next_line_new.c
@@ -0,0 +1,127 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Oceano <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/15 11:48:03 by Oceano            #+#    #+#             */
+/*   Updated: 2023/02/19 18:50:24 by utente           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <stdlib.h>
+#include <unistd.h>
+
+/*
+ * Polish linked list for next call
+*/
+void	polish_list(t_list **list)
+{
+	t_list	*last_node;
+	t_list	*clean_node;
+	char	*buf;
+	int		i;
+	int		k;
+
+	last_node = find_last_node(*list);
+	if (!last_node)
+		return ;
+	i = 0;
+	while (last_node->str_buf[i] && last_node->str_buf[i] != '\n')
+		i++;
+	if (!last_node->str_buf[i])
+		return ;
+	buf = malloc(BUFFER_SIZE + 1);
+	clean_node = malloc(sizeof(t_list));
+	if (!buf || !clean_node)
+		return ;
+	k = 0;
+	while (last_node->str_buf[++i])
+		buf[k++] = last_node->str_buf[i];
+	buf[k] = '\0';
+	clean_node->str_buf = buf;
+	clean_node->next = NULL;
+	dealloc(list, clean_node);
+}
+
+/*
+ * Get my (line\n] 
+*/
+char	*get_line(t_list *list)
+{
+	char	*next_str;
+	int		str_len;
+
+	if (!list)
+		return (NULL);
+	str_len = len_to_newline(list);
+	next_str = malloc(str_len + 1);
+	if (!next_str)
+		return (NULL);
+	copy_str(list, next_str);
+	return (next_str);
+}
+
+/*
+ * Append one node to the end of list
+*/
+void	append(t_list **list, char *buf)
+{
+	t_list	*new_node;
+	t_list	*last_node;
+
+	new_node = malloc(sizeof(t_list));
+	if (!new_node)
+		return ;
+	last_node = find_last_node(*list);
+	if (!last_node)
+		*list = new_node;
+	else
+		last_node->next = new_node;
+	new_node->str_buf = buf;
+	new_node->next = NULL;
+}
+
+/*
+ * Create list by reading file
+*/
+void	create_list(t_list **list, int fd)
+{
+	int		char_read;
+	char	*buf;
+
+	while (!found_newline(*list))
+	{
+		buf = malloc(BUFFER_SIZE + 1);
+		if (!buf)
+			return ;
+		char_read = read(fd, buf, BUFFER_SIZE);
+		if (char_read <= 0)
+		{
+			free(buf);
+			return ;
+		}
+		buf[char_read] = '\0';
+		append(list, buf);
+	}
+}
+
+/*
+ * Get next line from file
+*/
+char	*get_next_line(int fd)
+{
+	static t_list	*list = NULL;
+	char			*next_line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, &next_line, 0) < 0)
+		return (NULL);
+	create_list(&list, fd);
+	if (!list)
+		return (NULL);
+	next_line = get_line(list);
+	polish_list(&list);
+	return (next_line);
+}
