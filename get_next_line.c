88139b1f305986b87c94b29b6ba0711eadf1b894
MajaSta <142170733+MajaSta@users.noreply.github.com>
2024-09-29 14:21:46 +0200

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -30,7 +30,7 @@ void polish_list(t_list **list)
 }
 
 
-char *get_line(t_list *list)
+char *get_line(t_list *list) 
 {
     int str_len;
     char *next_str;
@@ -43,10 +43,10 @@ char *get_line(t_list *list)
         return (NULL);
     copy_str(list, next_str);
     return (next_str);
-
 }
 
-void append(t_list **list, char *buf)
+
+void append(t_list **list, char *buf) 
 {
     t_list *new_node;
     t_list *last_node;
@@ -54,10 +54,8 @@ void append(t_list **list, char *buf)
     new_node = malloc(sizeof(t_list));
     if (new_node == NULL)
         return;
-
-    new_node->str_buf = buf;
+    new_node->str_buf = strdup(buf); // Use strdup to copy buffer
     new_node->next = NULL;
-
     last_node = find_last_node(*list);
     if (last_node == NULL)
         *list = new_node;
@@ -65,20 +63,17 @@ void append(t_list **list, char *buf)
         last_node->next = new_node;
 }
 
-void create_list(t_list **list, int fd)
-{
+
+void create_list(t_list **list, int fd) {
     int char_read;
     char *buf;
 
-    while (!found_newline(*list))
-    {
+    while (!found_newline(*list)) {
         buf = malloc(BUFFER_SIZE + 1);
         if (buf == NULL)
             return;
-
         char_read = read(fd, buf, BUFFER_SIZE);
-        if (char_read <= 0)
-        {
+        if (char_read <= 0) {
             free(buf);
             if (char_read == 0) // EOF
                 return;
@@ -88,25 +83,20 @@ void create_list(t_list **list, int fd)
     }
 }
 
-
-char *get_next_line(int fd)
-{
+char *get_next_line(int fd) {
     static t_list *list = NULL;
     char *next_line;
 
     if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, NULL, 0) < 0)
-        return NULL;
-
+        return (NULL);
     create_list(&list, fd);
-
     if (list == NULL)
-        return NULL;
-
+        return (NULL);
     next_line = get_line(list);
     polish_list(&list);
-
-    return next_line;
+    return (next_line);
 }
+
 #include <fcntl.h>  // For open()
 #include <unistd.h> // For close()
 #include <stdio.h>  // For printf()
